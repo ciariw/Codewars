96e4d0225afb3c62b800b9acd9e8ce6f5cbc8afa
ciariw <90122269+ciariw@users.noreply.github.com>
2022-01-22 20:19:02 -0500

    Update Easy_supermarketQueue.js

--- js/Easy_supermarketQueue.js
+++ js/Easy_supermarketQueue.js
@@ -1,4 +1,4 @@
-//https://www.codewars.com/kata/57b06f90e298a7b53d000a86/train/python
+//https://www.codewars.com/kata/57b06f90e298a7b53d000a86
 /*
 There is a queue for the self-checkout tills at the supermarket. Your task is write a function to calculate the total time required for all the customers to check out!
 
