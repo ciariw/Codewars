c50b66600347929e1ff3ad04fb2fea7c0ea702c5
ciariw <90122269+ciariw@users.noreply.github.com>
2022-01-22 20:16:00 -0500

    Update and rename WhoLikesIt.js to Easy_WhoLikesIt.js

--- js/Easy_WhoLikesIt.js
+++ js/Easy_WhoLikesIt.js
@@ -1,4 +1,10 @@
 // https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/python
+/*
+You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. 
+We want to create the text that should be displayed next to such an item.
+
+Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+*/
 function likes(names) {
   let leng = names.length;
   let soln;
